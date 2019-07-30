86d1aa39f0174aa360aa221f65f59596f8ea370b
Anton Cheng <anton@coolbitx.com>
2019-07-30 14:53:42 +0800

    use basic auth ()updated package

--- src/transfer_confirmation.js
+++ src/transfer_confirmation.js
@@ -2,10 +2,11 @@ const { validateSchema } = require('./utils/ajv_validate');
 const { transferConfirmReqSchema } = require('./utils/schemas');
 const sygnaCrypto = require('./utils/crypto');
 const sygnaBridgeUtil = require('sygna-bridge-util');
-
 const { SygnaBridgeDomain } = require('../config');
-const API_KEY = process.env.API_KEY;
-if(!API_KEY) throw new Error('Missing API_KEY');
+const username = process.env.SB_USER;
+const password = process.env.SB_PWD;
+if (!username || !password) throw new Error("Missing SB_USER or SB_PWD. Please set them as environment variables.");
+const sygnaAPI = new sygnaBridgeUtil.api.API(username, password, SygnaBridgeDomain);
 
 /**
  * Reponse 200 if signature is valid and priv_info can be decoded successfully.
@@ -13,13 +14,13 @@ if(!API_KEY) throw new Error('Missing API_KEY');
  */
 async function transferConfirm (req_body) {
     validateSchema(req_body, transferConfirmReqSchema);
-    const { hex_data, transaction, originator_signature } = req_body;
+    const { hex_data, transaction, data_dt, originator_signature } = req_body;
     const { originator_vasp_code } = transaction;
     
     const originator_data = sygnaCrypto.decodePrivateInfo(hex_data);
     
-    const originator_pubKey = await sygnaBridgeUtil.api.getVASPPublicKey(SygnaBridgeDomain, API_KEY, originator_vasp_code);
-    const signObj = { hex_data , transaction};
+    const originator_pubKey = await sygnaAPI.getVASPPublicKey(originator_vasp_code, false);
+    const signObj = { hex_data , transaction, data_dt};
     sygnaCrypto.verifySignature(signObj, originator_pubKey, originator_signature);
     
     return originator_data;
@@ -45,7 +46,8 @@ async function validateAndCallBack(req_body, originator_data) {
     const result = "ACCEPT"; // or "REJECT"
     let params = { transfer_id, result };
     const beneficiary_signature = sygnaCrypto.signRequest(params);    
-    const finalresult = await sygnaBridgeUtil.api.beneficiary.callBackConfirmNotification(callback_url, API_KEY, transfer_id, result, beneficiary_signature);
+    const finalresult = await sygnaAPI.callBackConfirmNotification(transfer_id, result, beneficiary_signature);
+    // const finalresult = await sygnaBridgeUtil.api.beneficiary.callBackConfirmNotification(callback_url, API_KEY, transfer_id, result, beneficiary_signature);
     console.log(`Result from Sygna Bridge ${JSON.stringify(finalresult)}`);
 }
 
