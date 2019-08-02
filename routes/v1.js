a14a12933a6df358079f54df46c246b847052dbc
Anton Cheng <anton@coolbitx.com>
2019-08-02 09:20:30 +0800

    Remove api-key auth in router

--- routes/v1.js
+++ routes/v1.js
@@ -1,12 +1,9 @@
 const txConfirm = require('../src/transfer_confirmation');
-const { validateAPIKey } = require('../src/utils/api_key_validation');
 
 module.exports = ({ routerV1 }) => {
     routerV1.post('/transfer-confirmation', async (ctx) => {
         const reqBody = ctx.request.body;
-        validateAPIKey(ctx.request.header['api-key']);
-
-        let originator_data = await txConfirm.transferConfirm(reqBody, true);
+        let originator_data = await txConfirm.transferConfirm(reqBody);
         ctx.body = "success";
         
         // call back sygnaBridge when validation is done.
