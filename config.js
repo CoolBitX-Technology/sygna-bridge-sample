07fcdf62d373d0f851794946e29f53c72e3afd61
Anton Cheng <anton@coolbitx.com>
2019-08-08 14:43:10 +0800

    Update central server ip

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports ={
-    "SygnaBridgeDomain": "https://1zd393kqn5.execute-api.us-east-1.amazonaws.com/sygnabridge_staging/bridge/api"
+    "SygnaBridgeDomain": "https://1zd393kqn5.execute-api.us-east-1.amazonaws.com/stage/sb/api"
 };
     
